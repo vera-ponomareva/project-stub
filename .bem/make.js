25130b1d3bdaf5f2cc13b6c9201ea92ad571d548
Vladimir Grinenko <i@tadatuta.com>
2013-07-16 19:22:59 +0300

    create-browser.js+bemhtml-optimizer-node

--- .bem/make.js
+++ .bem/make.js
@@ -43,6 +43,12 @@ MAKE.decl('BundleNode', {
             'ie9.css',
             'html'
         ];
+    },
+
+    'create-browser.js+bemhtml-optimizer-node': function(tech, sourceNode, bundleNode) {
+        sourceNode.getFiles().forEach(function(f) {
+            this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
+        }, this);
     }
 
 });
